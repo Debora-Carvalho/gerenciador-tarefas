d698f5600406411f3a26d1e71639453a46f9e60e
Debora-Carvalho <deboravolc2@gmail.com>
2025-05-20 09:54:21 -0300

    adicionar o fim das rotas

--- src/controller/tarefasController.js
+++ src/controller/tarefasController.js
@@ -7,8 +7,8 @@ const mongoose = require("mongoose");
 require("../models/tarefas.js");
 const Tarefas = mongoose.model("tarefas");
 
-// __________Rotas das tarefas __________
-// 2) Abre e carrega todas as informações de tarefas no formulário tarefas.handlebars
+/* __________Rotas das tarefas __________*/
+/* 2) Abre e carrega todas as informações de tarefas no formulário tarefas.handlebars */
 
 router.get('/tarefas', (req, res) => {
     Tarefas.find().lean().then((tarefas) => {
@@ -16,13 +16,13 @@ router.get('/tarefas', (req, res) => {
     });
 });
 
-// 3) Abre o formulário addtarefas.handlebars
+/* 3) Abre o formulário addtarefas.handlebars */
 router.get('/tarefas/add', (req, res) => {
     res.render("admin/tarefas/addtarefas");
 });
 
-// 4) Recebe as informações do botão que está no addtarefas.handlebars 
-//e efetua o cadastro no Banco de dados, depois ele volta para a listagem das tarefas
+/* 4) Recebe as informações do botão que está no addtarefas.handlebars 
+e efetua o cadastro no Banco de dados, depois ele volta para a listagem das tarefas */
 router.post('/tarefas/nova', (req, res) => {
     var tarefas = new Tarefas();
     tarefas.nome = req.body.nome;
@@ -34,15 +34,15 @@ router.post('/tarefas/nova', (req, res) => {
     });
 });
 
-// 5) Abre e preenche o formulário edittarefas.handlebars com informações do id passado
+/* 5) Abre e preenche o formulário edittarefas.handlebars com informações do id passado */
 router.get('/editar_tarefas/:id', (req, res) => {
     Tarefas.findOne({ _id: req.params.id }).lean().then((tarefas) => {
         res.render("admin/tarefas/edittarefas", { tarefa: tarefas });
     });
 });
 
-// 6) Recebe as informações do botão que está no edittarefa.handlebar e efetua a alteração
-//no banco de dados. Volta para a listagem das tarefas
+/* 6) Recebe as informações do botão que está no edittarefa.handlebar e efetua a alteração
+no banco de dados. Volta para a listagem das tarefas */
 router.post('/tarefas/editar_tarefas', (req, res) => {
     Tarefas.updateOne({ _id: req.body._id },
     {
@@ -55,5 +55,18 @@ router.post('/tarefas/editar_tarefas', (req, res) => {
     })
 });
 
-// 7) No form turma.handlebars que lista as turmas possui um botão para deletar
-//Ele deleta informações e refaz a lista no 
+/* 7) No form turma.handlebars que lista as turmas possui um botão para deletar
+Ele deleta informações e refaz a lista no turma.handlebars */
+router.get('/deletar_tarefas/:id', (req, res) => {
+    Tarefas.deleteMany({ _id: req.params.id }).then(() => {
+        res.redirect("/rota_tarefas/tarefas");
+    });
+});
+
+/* __________Fim das rotas das tarefas __________*/
+module.exports = router;
+
+
+
+
+
